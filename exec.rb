673765eff8a0d19c8377e911cb14edf4a312a050
ku70t6h1k6r1 <uju5150@gmail.com>
2017-11-23 11:23:52 +0900

    create geitopicom.rb
    first commit

--- exec.rb
+++ exec.rb
@@ -7,22 +7,22 @@ require 'date'
 dt = DateTime.now
 
 time = 1
-while time < 6418
+while time < 609
 	
 	begin
 		#dtStr = dt.strftime('%Y-%m-%d')
-		@tempUrl = "http://www.elle.co.jp/culture/celebgossip/(offset)/#{time}"
+		@tempUrl = "https://geitopi.com/page/#{time}/"
 		doc = Nokogiri.HTML(open(@tempUrl, :ssl_verify_mode => OpenSSL::SSL::VERIFY_NONE).read)
 
-		doc.xpath("//ul[@class='article-list-type-a']/li/a").each do |u|
-			@url = "http://www.elle.co.jp"+ u["href"] 
+		doc.xpath("//div[@class='entry']/a").each do |u|
+			@url =  u["href"] 
 			puts @url
 			begin
 				doc2 = Nokogiri.HTML(open(@url, :ssl_verify_mode => OpenSSL::SSL::VERIFY_NONE).read)
 			#	
-				puts "dt:" + doc2.xpath("//span[@class='label-date']")[0].inner_text
-				puts "title:" + doc2.xpath("//header[@class='title-main']/h2")[0].inner_text
-				puts "body:" + doc2.xpath("//section[@id='area-entry']").inner_text.gsub(/\n/,"")
+				puts "dt:" + doc2.xpath("//div[@class='post-date']")[0].inner_text
+				puts "title:" + doc2.xpath("//h2[@class='post-title']")[0].inner_text
+			#	puts "body:" + doc2.xpath("//div[@class='article']").inner_text.gsub(/\n/,"")
 			#	prof1 = doc2.xpath("//div[@id='profData']").inner_text
 			#	prof2 = doc2.xpath("//div[@id='myMessage']").inner_text
 			#	prof3 = doc2.xpath("//div[@id='profileList']").inner_text
@@ -34,5 +34,5 @@ while time < 6418
 	rescue => e
 		puts e
 	end
-	time = time + 8
+	time = time + 1
 end
---
+++ geitopicom.rb
@@ -0,0 +1,89 @@
+require 'nokogiri'
+require 'net/https'
+require 'open-uri'
+require 'date'
+
+#######################
+
+class SqlSet
+	def insert(client,m_n, u, t, b, c_n, e1, e5)
+		client.query(
+			" INSERT INTO crawler_raw_data_for_test (
+				  media_name
+				, url
+				, title
+				, body
+				, crawler_name
+				, etc1
+				, etc5
+				)
+			VALUES(
+				\"#{m_n}\"
+				,\"#{u}\"
+				,\"#{t}\"
+				,LEFT(\"#{b}\", 9999)
+				,\"#{c_n}\"
+				,\"#{e1}\"
+				,\"#{e5}\"
+				)
+			 "
+		)
+	end
+end
+
+
+#######################
+@client = Mysql2::Client.new(:host => "", :username => "", :password => "", :database => "")
+@sql = SqlSet.new
+@url = ""
+@title = ""
+@body = ""
+@media_name = "geitopicom"
+@crawler_name = "geitopicomRB"
+@etc1 = ""
+@etc5 = ""
+
+time = 1
+while time < 609
+	
+	begin
+		@tempUrl = "https://geitopi.com/page/#{time}/"
+		doc = Nokogiri.HTML(open(@tempUrl, :ssl_verify_mode => OpenSSL::SSL::VERIFY_NONE).read)
+
+		doc.xpath("//div[@class='entry']/a").each do |u|
+			@url =  u["href"] 
+			puts @url
+			begin
+				sleep(2)
+				doc2 = Nokogiri.HTML(open(@url, :ssl_verify_mode => OpenSSL::SSL::VERIFY_NONE).read)
+
+				@etc1 = ""
+				@title = ""
+				@body = ""
+				@etc5 = ""
+				@etc1 = doc2.xpath("//div[@class='post-date']")[0].inner_text
+				@title = doc2.xpath("//h2[@class='post-title']")[0].inner_text
+				@body = doc2.xpath("//div[@class='article']").inner_text.gsub(/\n/,"")
+				begin
+					@sql.insert(@client, @media_name, @url, @title, @body, @crawler_name, @etc1, @etc5)
+				rescue =>e
+					puts e
+				end	
+			rescue =>e
+				puts e
+				begin
+					@etc1 = ""
+					@title = ""
+					@body = ""
+					@etc5 = "error"
+					@sql.insert(@client, @media_name,@url, @title, @body, @crawler_name, @etc1, @etc5)
+				rescue => e
+					puts e
+				end	
+			end
+		end
+	rescue => e
+		puts e
+	end
+	time = time + 1
+end
