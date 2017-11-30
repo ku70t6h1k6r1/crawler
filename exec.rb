1fd2e4d1a8a30f04a1b4431359c90755bdd2035a
ku70t6h1k6r1 <uju5150@gmail.com>
2017-12-01 00:23:30 +0900

    create mnsatlascom.rb
    first commit

--- exec.rb
+++ exec.rb
@@ -6,22 +6,22 @@ require 'date'
 dt = DateTime.now
 
 time = 1
-while time < 1298
+while time < 2
 	
 	begin
 		#dtStr = dt.strftime('%Y-%m-%d')
-		@tempUrl = "http://labaq.com/?p=#{time}/"
+		@tempUrl = "http://mnsatlas.com/?cat=17&paged=1/"
 		doc = Nokogiri.HTML(open(@tempUrl, :ssl_verify_mode => OpenSSL::SSL::VERIFY_NONE).read)
 
-		doc.xpath("//h2[@class='title']/a").each do |u|
+		doc.xpath("//h4[@class='title']/a").each do |u|
 			@url =   u["href"] 
 			puts @url
 			begin
 				doc2 = Nokogiri.HTML(open(@url, :ssl_verify_mode => OpenSSL::SSL::VERIFY_NONE).read)
 			#	
-				puts "dt:" + doc2.xpath("//div[@class='date']")[0].inner_text
-				puts "title:" + doc2.xpath("//h1[@class='title']")[0].inner_text
-				puts "body:" + doc2.xpath("//div[@class='main']")[0].inner_text.gsub(/\n/,"") + doc2.xpath("//div[@class='mainmore']")[0].inner_text.gsub(/\n/,"")
+				puts "dt:" + doc2.xpath("//li[@class='date']")[0].inner_text
+				puts "title:" + doc2.xpath("//div[@id='single_title']/h2").inner_text
+				puts "body:\n" + doc2.xpath("//div[@class='post clearfix']").inner_text.gsub(/\n/,"") 
 			#	prof1 = doc2.xpath("//div[@id='profData']").inner_text
 			#	prof2 = doc2.xpath("//div[@id='myMessage']").inner_text
 			#	prof3 = doc2.xpath("//div[@id='profileList']").inner_text
---
+++ mnsatlascom.rb
@@ -0,0 +1,315 @@
+require 'nokogiri'
+require 'net/https'
+require 'open-uri'
+require 'date'
+require 'mysql2'
+
+#######################
+
+class SqlSet
+	def insert(client,m_n, u, t, b, c_n, e1, e5)
+		client.query(
+			" INSERT INTO crawler_raw_data_for_test (
+				  media_name
+				, url
+				, title
+				, body
+				, crawler_name
+				, etc1
+				, etc5
+				)
+			VALUES(
+				\"#{m_n}\"
+				,\"#{u}\"
+				,\"#{t}\"
+				,LEFT(\"#{b}\", 9999)
+				,\"#{c_n}\"
+				,\"#{e1}\"
+				,\"#{e5}\"
+				)
+			 "
+		)
+	end
+end
+
+
+#######################
+@client = Mysql2::Client.new(:host => "", :username => "", :password => "", :database => "")
+@sql = SqlSet.new
+@url = ""
+@title = ""
+@body = ""
+@media_name = "mnsatlascom"
+@crawler_name = "mnsatlascomRB"
+@etc1 = ""
+@etc5 = ""
+
+#######################
+time = 1
+while time < 2
+	begin
+		@tempUrl = "http://mnsatlas.com/?cat=17&paged=#{time}"
+		doc = Nokogiri.HTML(open(@tempUrl, :ssl_verify_mode => OpenSSL::SSL::VERIFY_NONE).read)
+
+		doc.xpath("//h4[@class='title']/a").each do |u|
+			@url =   u["href"] 
+			puts @url
+			begin
+				sleep(2)
+				doc2 = Nokogiri.HTML(open(@url, :ssl_verify_mode => OpenSSL::SSL::VERIFY_NONE).read)
+
+				@etc1 = ""
+				@title = ""
+				@body = ""
+				@etc5 = ""
+				@etc1 = doc2.xpath("//li[@class='date']")[0].inner_text
+				@title = doc2.xpath("//div[@id='single_title']/h2").inner_text
+				@body = doc2.xpath("//div[@class='post clearfix']").inner_text.gsub(/\n/,"")
+				begin
+					@sql.insert(@client, @media_name, @url, @title, @body, @crawler_name, @etc1, @etc5)
+				rescue =>e
+					puts e
+				end		
+			rescue =>e
+				puts e
+				begin
+					@etc1 = ""
+					@title = ""
+					@body = ""
+					@etc5 = "error"
+					@sql.insert(@client, @media_name,@url, @title, @body, @crawler_name, @etc1, @etc5)
+				rescue => e
+					puts e
+				end	
+			end
+		end
+	rescue => e
+		puts e
+	end
+	time = time + 1
+end
+
+#######################
+time = 1
+while time < 2
+	begin
+		@tempUrl = "http://mnsatlas.com/?cat=23&paged=1/#{time}"
+		doc = Nokogiri.HTML(open(@tempUrl, :ssl_verify_mode => OpenSSL::SSL::VERIFY_NONE).read)
+
+		doc.xpath("//h4[@class='title']/a").each do |u|
+			@url =   u["href"] 
+			puts @url
+			begin
+				sleep(2)
+				doc2 = Nokogiri.HTML(open(@url, :ssl_verify_mode => OpenSSL::SSL::VERIFY_NONE).read)
+
+				@etc1 = ""
+				@title = ""
+				@body = ""
+				@etc5 = ""
+				@etc1 = doc2.xpath("//li[@class='date']")[0].inner_text
+				@title = doc2.xpath("//div[@id='single_title']/h2").inner_text
+				@body = doc2.xpath("//div[@class='post clearfix']").inner_text.gsub(/\n/,"")
+				begin
+					@sql.insert(@client, @media_name, @url, @title, @body, @crawler_name, @etc1, @etc5)
+				rescue =>e
+					puts e
+				end		
+			rescue =>e
+				puts e
+				begin
+					@etc1 = ""
+					@title = ""
+					@body = ""
+					@etc5 = "error"
+					@sql.insert(@client, @media_name,@url, @title, @body, @crawler_name, @etc1, @etc5)
+				rescue => e
+					puts e
+				end	
+			end
+		end
+	rescue => e
+		puts e
+	end
+	time = time + 1
+end
+
+#######################
+time = 1
+while time < 2
+	begin
+		@tempUrl = "http://mnsatlas.com/?cat=25&paged=1/#{time}"
+		doc = Nokogiri.HTML(open(@tempUrl, :ssl_verify_mode => OpenSSL::SSL::VERIFY_NONE).read)
+
+		doc.xpath("//h4[@class='title']/a").each do |u|
+			@url =   u["href"] 
+			puts @url
+			begin
+				sleep(2)
+				doc2 = Nokogiri.HTML(open(@url, :ssl_verify_mode => OpenSSL::SSL::VERIFY_NONE).read)
+
+				@etc1 = ""
+				@title = ""
+				@body = ""
+				@etc5 = ""
+				@etc1 = doc2.xpath("//li[@class='date']")[0].inner_text
+				@title = doc2.xpath("//div[@id='single_title']/h2").inner_text
+				@body = doc2.xpath("//div[@class='post clearfix']").inner_text.gsub(/\n/,"")
+				begin
+					@sql.insert(@client, @media_name, @url, @title, @body, @crawler_name, @etc1, @etc5)
+				rescue =>e
+					puts e
+				end		
+			rescue =>e
+				puts e
+				begin
+					@etc1 = ""
+					@title = ""
+					@body = ""
+					@etc5 = "error"
+					@sql.insert(@client, @media_name,@url, @title, @body, @crawler_name, @etc1, @etc5)
+				rescue => e
+					puts e
+				end	
+			end
+		end
+	rescue => e
+		puts e
+	end
+	time = time + 1
+end
+
+#######################
+time = 1
+while time < 2
+	begin
+		@tempUrl = "http://mnsatlas.com/?cat=3&paged=1/#{time}"
+		doc = Nokogiri.HTML(open(@tempUrl, :ssl_verify_mode => OpenSSL::SSL::VERIFY_NONE).read)
+
+		doc.xpath("//h4[@class='title']/a").each do |u|
+			@url =   u["href"] 
+			puts @url
+			begin
+				sleep(2)
+				doc2 = Nokogiri.HTML(open(@url, :ssl_verify_mode => OpenSSL::SSL::VERIFY_NONE).read)
+
+				@etc1 = ""
+				@title = ""
+				@body = ""
+				@etc5 = ""
+				@etc1 = doc2.xpath("//li[@class='date']")[0].inner_text
+				@title = doc2.xpath("//div[@id='single_title']/h2").inner_text
+				@body = doc2.xpath("//div[@class='post clearfix']").inner_text.gsub(/\n/,"")
+				begin
+					@sql.insert(@client, @media_name, @url, @title, @body, @crawler_name, @etc1, @etc5)
+				rescue =>e
+					puts e
+				end		
+			rescue =>e
+				puts e
+				begin
+					@etc1 = ""
+					@title = ""
+					@body = ""
+					@etc5 = "error"
+					@sql.insert(@client, @media_name,@url, @title, @body, @crawler_name, @etc1, @etc5)
+				rescue => e
+					puts e
+				end	
+			end
+		end
+	rescue => e
+		puts e
+	end
+	time = time + 1
+end
+
+#######################
+time = 1
+while time < 2
+	begin
+		@tempUrl = "http://mnsatlas.com/?cat=33&paged=1/#{time}"
+		doc = Nokogiri.HTML(open(@tempUrl, :ssl_verify_mode => OpenSSL::SSL::VERIFY_NONE).read)
+
+		doc.xpath("//h4[@class='title']/a").each do |u|
+			@url =   u["href"] 
+			puts @url
+			begin
+				sleep(2)
+				doc2 = Nokogiri.HTML(open(@url, :ssl_verify_mode => OpenSSL::SSL::VERIFY_NONE).read)
+
+				@etc1 = ""
+				@title = ""
+				@body = ""
+				@etc5 = ""
+				@etc1 = doc2.xpath("//li[@class='date']")[0].inner_text
+				@title = doc2.xpath("//div[@id='single_title']/h2").inner_text
+				@body = doc2.xpath("//div[@class='post clearfix']").inner_text.gsub(/\n/,"")
+				begin
+					@sql.insert(@client, @media_name, @url, @title, @body, @crawler_name, @etc1, @etc5)
+				rescue =>e
+					puts e
+				end		
+			rescue =>e
+				puts e
+				begin
+					@etc1 = ""
+					@title = ""
+					@body = ""
+					@etc5 = "error"
+					@sql.insert(@client, @media_name,@url, @title, @body, @crawler_name, @etc1, @etc5)
+				rescue => e
+					puts e
+				end	
+			end
+		end
+	rescue => e
+		puts e
+	end
+	time = time + 1
+end
+
+#######################
+time = 1
+while time < 2
+	begin
+		@tempUrl = "http://mnsatlas.com/?cat=6&paged=1/#{time}"
+		doc = Nokogiri.HTML(open(@tempUrl, :ssl_verify_mode => OpenSSL::SSL::VERIFY_NONE).read)
+
+		doc.xpath("//h4[@class='title']/a").each do |u|
+			@url =   u["href"] 
+			puts @url
+			begin
+				sleep(2)
+				doc2 = Nokogiri.HTML(open(@url, :ssl_verify_mode => OpenSSL::SSL::VERIFY_NONE).read)
+
+				@etc1 = ""
+				@title = ""
+				@body = ""
+				@etc5 = ""
+				@etc1 = doc2.xpath("//li[@class='date']")[0].inner_text
+				@title = doc2.xpath("//div[@id='single_title']/h2").inner_text
+				@body = doc2.xpath("//div[@class='post clearfix']").inner_text.gsub(/\n/,"")
+				begin
+					@sql.insert(@client, @media_name, @url, @title, @body, @crawler_name, @etc1, @etc5)
+				rescue =>e
+					puts e
+				end		
+			rescue =>e
+				puts e
+				begin
+					@etc1 = ""
+					@title = ""
+					@body = ""
+					@etc5 = "error"
+					@sql.insert(@client, @media_name,@url, @title, @body, @crawler_name, @etc1, @etc5)
+				rescue => e
+					puts e
+				end	
+			end
+		end
+	rescue => e
+		puts e
+	end
+	time = time + 1
+end
