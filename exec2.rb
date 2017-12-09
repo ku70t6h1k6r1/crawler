e07743d7dd87f321f57e1d2611802503d7e55d3b
Cloud User <ec2-user@ip-172-31-57-200.ec2.internal>
2017-12-09 13:49:26 +0900

    INSERT INTO WATAHIHA SERVER COMPELTED

--- exec2.rb
+++ exec2.rb
@@ -6,14 +6,14 @@ require 'mysql2'
 
 
 gateway = Net::SSH::Gateway.new(
-  '',
+  '...',
   '',
   port:, 
   keys: ['/home/ec2-user/key/']
 
 )
 
-gateway.open() do |local_port|
+gateway.open('127.0.0.1', 3306) do |local_port|
 
   client = Mysql2::Client.new(
     host: '127.0.0.1',
@@ -24,7 +24,7 @@ gateway.open() do |local_port|
   )
 
   clientEC2 = Mysql2::Client.new(
-    host: '',
+    host: '127.0.0.1',
     username: '',
     password: '',
     database: ''
@@ -35,7 +35,7 @@ gateway.open() do |local_port|
 	@body = ""
 	@body = client.escape(reader["body"])
 	begin
-		client.query("INSERT INTO test_crawler_data_cleaned (etc1, media_name, url, title, body, crawler_name)
+		client.query("INSERT INTO crawler_data_cleaned (etc1, media_name, url, title, body, crawler_name)
 		VALUES (\"#{reader["serial"]}\", \"#{reader["media_name"]}\", \"#{reader["url"]}\", \"#{reader["title"]}\", \"#{@body}\", \"#{reader["crawler_name"]}\");")
 	rescue => e
 		puts e
